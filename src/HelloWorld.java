a21d06dd087d1dab95a516a43f7493054b0714f4
fazilsheriff <fazilsheriff@gmail.com>
2023-12-18 17:01:18 +0530

    List intro - List of

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 import java.security.DomainCombiner;
+import java.util.List;
 
 public class HelloWorld {
 	public static void main(String[] args) {
 	
-		System.out.println("Hello- World");
+	System.out.println("Hello-World");
+
+		
+		
 	}
 
 }
---
+++ src/List/ListCreateFunctions.java
@@ -0,0 +1,14 @@
+package List;
+
+import java.util.List;
+
+public class ListCreateFunctions {
+public static void main(String[] args) {
+	List<String> words=List.of("Apple","Banana","citrus","dol");
+	System.out.println(words);
+	//When try to add list.of you get exception
+	//java.lang.UnsupportedOperationException
+	words.add("elegant");
+	System.out.println(words);
+}
+}
