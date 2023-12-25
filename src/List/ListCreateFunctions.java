c6cb72a2b4cc6bdcd23c4e3a2d277a6f20ac5ea8
fazilsheriff <fazilsheriff@gmail.com>
2023-12-25 19:27:29 +0530

    functions in lists

--- src/List/ListCreateFunctions.java
+++ src/List/ListCreateFunctions.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 public class ListCreateFunctions {
 public static void main(String[] args) {
-	List<String> words=List.of("Apple","Banana","citrus","dol");
+	List<String> words=List.of("Apple","Banana");
 	System.out.println(words);
-	//When try to add list.of you get exception
-	//java.lang.UnsupportedOperationException
-	words.add("elegant");
-	System.out.println(words);
-	//FAZIL
 	//now wroking on functions of lists 
+	//Length-size
+	System.out.println("Lenth is "+words.size());
+	//check is list is empty or not 
+	System.out.println("List  isEmmpty "+words.isEmpty());
+	//contains
+	System.out.println("is list contains Apple?"+words.contains("Apple"));
+	//index
+	System.out.println("index of "+words.get(0));
+//	//IF index is not found
+//	System.out.println("If index is not found "+words.get(8));
 }
 }
