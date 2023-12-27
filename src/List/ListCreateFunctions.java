3357a882d3d7647c5d674d2e41bcc24be18fde1f
fazilsheriff <fazilsheriff@gmail.com>
2023-12-27 08:04:18 +0530

    Add remove methods are added

--- src/List/ListCreateFunctions.java
+++ src/List/ListCreateFunctions.java
@@ -8,26 +8,44 @@ import java.util.Vector;
 public class ListCreateFunctions {
 public static void main(String[] args) {
 	List<String> words=List.of("Apple","Banana");
-	System.out.println(words);
-	//now wroking on functions of lists 
-	//Length-size
-	System.out.println("Lenth is "+words.size());
-	//check is list is empty or not 
-	System.out.println("List  isEmmpty "+words.isEmpty());
-	//contains
-	System.out.println("is list contains Apple?"+words.contains("Apple"));
-	//index
-	System.out.println("index of "+words.get(0));
-//	//IF index is not found
-//	System.out.println("If index is not found "+words.get(8));
+	System.out.println("Lists"+words);
+
 	
 	//Creating new ArrayLists,Linked Lists and Vector
 	List<String> al_words=new ArrayList<String>(words);
+//	List<String> ll_words=new LinkedList<String>(words);
+//	System.out.println("List is converted in LinkedList"+ll_words);
+//	List<String> vector_words=new Vector<String>(words);
+//	System.out.println("List is converted in Vector"+vector_words);
+
+// Add new items to the list 
+	al_words.add("Cake");
+	al_words.add("Donut");
 	System.out.println("List is converted in ArrayList"+al_words);
-	List<String> ll_words=new LinkedList<String>(words);
-	System.out.println("List is converted in LinkedList"+ll_words);
-	List<String> vector_words=new Vector<String>(words);
-	System.out.println("List is converted in Vector"+vector_words);
+	//Allows duplications for n number of times
+	al_words.add("Cake");
+	System.out.println("List Allows duplication"+al_words);
+	
+	//Removing item 
+	al_words.remove("Cake");
+	System.out.println("Removing an item"+al_words);
+	
+	//Adding an element in specific location
+	al_words.add(2, "Cake");
+	System.out.println("Adding an element in specific location"+al_words);
+
+
+	// Removing an item in list by id
+	al_words.remove(2);
+	System.out.println("Removing an element in specific location"+al_words);
+	
+	//Removing by string
+	al_words.remove("Cake");
+	System.out.println("Removing an element by string"+al_words);
+	
+	//Removing all elements in lists
+	al_words.removeAll(words);
+	System.out.println("Removing an element by string"+al_words);
 
 
 
